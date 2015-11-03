5600dd76dd1b024392372653cd96f7dd99230635
olagos <info.olagos@gmail.com>
2015-11-03 23:54:24 +0100

    Update obpost.groovy

---
+++ src/com/somonar/orrtiz/shipment/obpost/obpost.groovy
@@ -0,0 +1,3 @@
+Document itemDocument = UtilXml.makeEmptyXmlDocument("BPostDoc");
+Element BPostDocElem = itemDocument.getDocumentElement();
+BPostElem.setAttribute("xmlns", "urn:ebay:apis:eBLBaseComponents");
